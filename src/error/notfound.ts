30840425ca03417c46288d625f11ef4db26d72b2
magocod <magoyeison@gmail.com>
2022-09-18 07:46:21 -0400

    remove postgres data

--- src/error/notfound.ts
+++ src/error/notfound.ts
@@ -2,7 +2,7 @@ import { BaseError } from "./base";
 
 export class UnImplementedError extends BaseError {
   constructor() {
-    super("unimplemented", 400, "unimplemented");
+    super("unimplemented", 404, "unimplemented");
     this.name = "UnImplementedError";
   }
 }
